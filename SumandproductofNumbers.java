c4efa538057cd5f2f8e766053e650848b3083dd0
mohanrajtamilmaran <150466483+mohanrajtamilmaran@users.noreply.github.com>
2023-11-11 16:30:10 +0530

    Sum and product of Numbers

---
+++ SumandproductofNumbers.java
@@ -0,0 +1,18 @@
+package Day2;
+
+public class SumandproductofNumbers {
+	public static void main(String[] args)
+	{
+		int n,num=34,sum=0,product=1;
+		while(num>0)
+		{
+			n=num %10;//rem of the num will be stored in n n=4
+			sum  = sum + n; //Sum=0+4 as n=4 now sum = 4
+			product = product * n; //product = 1*4 as n=4 now product = 4
+			num = num /10; // num = 34/10 will return first number now num = 3
+		}
+		System.out.println("Sum of Digits:"+sum);
+		System.out.println("Product of the digits:"+product);
+	}
+
+}
